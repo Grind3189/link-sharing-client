c3b37cc873158175e77216b28a41c151ccfa1a81
Grind <bellenlowell@gmail.com>
2023-09-22 18:57:16 +0800

    added login link to nav

---
+++ src/components/nav/GetLogin.tsx
@@ -0,0 +1,34 @@
+interface GetLoginProp {
+  width: number;
+  hover: string;
+}
+const GetLoginIc = ({ width, hover }: GetLoginProp) => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke={hover === 'login' ? "#633CFF" : "#737373"}
+        width="21"
+        height="20"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+        />
+      </svg>
+      {width >= 768 && (
+        <span
+          className={`font-semibold ${hover === 'login' ? 'text-purple-300' : 'text-grey-200'}`}
+        >
+          Login
+        </span>
+      )}
+    </>
+  );
+};
+
+export default GetLoginIc;
--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -6,46 +6,64 @@ import GetProfileIc from "./GetProfileIc";
 import { Link, useLocation } from "react-router-dom";
 import { useState, useContext } from "react";
 import { WidthContext } from "../../context/WidthContextProvider";
+import GetLoginIc from "./GetLogin";
 const Navbar = () => {
   const [hover, setHover] = useState<string>("");
-  const currentPath = useLocation().pathname
-  const [activeNav, setActiveNav] = useState<string>(currentPath === '/' ? 'home' : 'profile');
-  const {width} = useContext(WidthContext)
+  const currentPath = useLocation().pathname;
+  const [activeNav, setActiveNav] = useState<string>(
+    currentPath === "/" ? "home" : "profile",
+  );
+  const { width } = useContext(WidthContext);
   const gridCenter = `grid place-items-center`;
-
 
   return (
     <header className={`bg-white px-6 py-4 ${gridCenter} h-full`}>
       <nav className="flex h-[42px] w-full items-center">
-        <Link to="." className="" onClick={() => setActiveNav('home')}>
+        <Link to="." onClick={() => setActiveNav("home")}>
           <img src={width < 768 ? logoSm : logoLg} alt="" />
         </Link>
 
-        <div className="mx-auto flex h-full">
+        <div className="flex h-full mx-auto">
           <Link
             to="."
-            className={`${gridCenter} gap-2 rounded-lg px-[27px] md:flex ${activeNav === 'home' && 'bg-purple-100'}`}
+            className={`${gridCenter} gap-2 rounded-lg px-[27px] md:flex ${
+              activeNav === "home" && "bg-purple-100"
+            }`}
             onMouseEnter={() => setHover("home")}
             onMouseLeave={() => setHover("")}
-            onClick={() => setActiveNav('home')}
+            onClick={() => setActiveNav("home")}
           >
             <GetLinkIc activeNav={activeNav} width={width} hover={hover} />
           </Link>
           <Link
             to="profile"
-            className={`${gridCenter} gap-2 rounded-lg px-[27px] md:flex ${activeNav === 'profile' && 'bg-purple-100'}`}
+            className={`${gridCenter} gap-2 rounded-lg px-[27px] md:flex ${
+              activeNav === "profile" && "bg-purple-100"
+            }`}
             onMouseEnter={() => setHover("profile")}
             onMouseLeave={() => setHover("")}
-            onClick={() => setActiveNav('profile')}
+            onClick={() => setActiveNav("profile")}
           >
             <GetProfileIc activeNav={activeNav} width={width} hover={hover} />
           </Link>
+          <Link
+            to="login"
+            className={`${gridCenter} gap-2 rounded-lg px-[27px] md:flex ${
+              activeNav === "login" && "bg-purple-100"
+            }`}
+            onMouseEnter={() => setHover("login")}
+            onMouseLeave={() => setHover("")}
+            onClick={() => setActiveNav('')}
+          >
+            <GetLoginIc width={width} hover={hover} />
+          </Link>
+          
         </div>
 
         <Link
           to="preview"
           className={`h-full border border-purple-300 px-4 ${gridCenter} rounded-lg hover:bg-purple-100`}
-          onClick={() => setActiveNav('')}
+          onClick={() => setActiveNav("")}
         >
           {width >= 768 ? (
             <span className="font-semibold text-purple-300">Preview</span>
